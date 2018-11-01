e7253a7e9d9cbb1608434d7cf3ea6f1590ab3c21
Leticia-moura <44246524+Leticia-moura@users.noreply.github.com>
2018-11-01 20:47:33 -0300

    Add files via upload

---
+++ aula16/017_VerdadeiroFalso/src/controller/teste.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class teste {
+
+	public static void main(String[] args) {
+		boolean b=false;
+		boolean a=true;
+		if(a==b){
+		System.out.println("iguais");
+		}
+		else {
+			System.out.println("diferentes");
+			
+		}
+
+	}
+
+}
